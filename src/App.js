9aeabee12261ad41a486be6e7b9052e52b0034c2
Prabeen04 <prabeen.strange@gmail.com>
2018-07-04 21:17:04 +0530

    external data removed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import BarChart from './components/barChart';
-import data from './components/data';
 class App extends Component {
   constructor(props){
     super(props)
-
-    this.state = { screenWidth: 1000, screenHeight: 500, hover: "none", brushExtent: [0,40] }
   }
   render() {
     return (
