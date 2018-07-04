e333bff339486f17f2bfab80da305d66ff5f5adc
Prabeen04 <prabeen.strange@gmail.com>
2018-07-04 21:06:38 +0530

    state added in app and passed to BarChart

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import BarChart from './components/barChart';
 import data from './components/data';
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = { screenWidth: 1000, screenHeight: 500, hover: "none", brushExtent: [0,40] }
+  }
   render() {
-    console.log(d3-scale)
     return (
       <div className="App">
-        <BarChart data={data}/>
+        <BarChart data={data} size={[this.state.screenWidth / 2, this.state.screenHeight / 2]}/>
       </div>
     );
   }
--- src/components/barChart.js
+++ src/components/barChart.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import { scaleLinear } from 'd3-scale';
 import { max } from 'd3-array';
 import { select } from 'd3-selection';
