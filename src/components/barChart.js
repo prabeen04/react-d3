ffc072fe074ab8409c43ea66d46498dcb8d46d12
Prabeen04 <prabeen.strange@gmail.com>
2018-07-04 21:33:03 +0530

    small suff

--- src/components/barChart.js
+++ src/components/barChart.js
@@ -36,7 +36,7 @@ class BarChart extends Component {
    select(node)
       .selectAll('rect')
       .data(this.props.data)
-      .style('fill', '#fe9922')
+      .style('fill', 'tomato')
       .attr('x', (d,i) => i * 25)
       .attr('y', d => this.props.size[1] - yScale(d))
       .attr('height', d => yScale(d))
