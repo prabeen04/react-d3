f12571ceee989d49dd935bab2cfb94d25716c049
Prabeen04 <prabeen.strange@gmail.com>
2018-07-04 20:52:57 +0530

    more d3 packages added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import { d3-scale } from 'd3';
 
 class App extends Component {
   render() {
+    console.log(d3-scale)
     return (
       <div className="App">
       </div>
